406431d503cf6d1891d17b76d076547b705232af
Raph_Lem <raphael.lemoine@edu.univ-fcomte.fr>
2022-11-18 17:37:35 +0100

    Creation Connexion et inscription

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.esm.js";
+
+import "@popperjs";
